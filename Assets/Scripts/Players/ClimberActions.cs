6f6316d92c30ab38342f914e660014f90b9d755b
Thomas <t.dluhosch@gmail.com>
2026-01-16 22:42:24 +0100

    add climber scripts

---
+++ Assets/Scripts/Players/ClimberActions.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ClimberActions : MonoBehaviour
+{
+
+    // animations in Update()
+    void Update()
+    {
+        
+    }
+
+    // todo
+    public void Climb()
+    {
+        
+    }
+
+    // todo
+    public void Drag()
+    {
+        
+    }
+
+}
