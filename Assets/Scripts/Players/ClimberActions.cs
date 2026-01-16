b3ed80fc2e4e56fc2733e2a20ddaa0f0286c0da7
Thomas <t.dluhosch@gmail.com>
2026-01-16 23:08:26 +0100

    add climbercontroller and actions

--- Assets/Scripts/Players/ClimberActions.cs
+++ Assets/Scripts/Players/ClimberActions.cs
@@ -3,22 +3,52 @@ using UnityEngine;
 public class ClimberActions : MonoBehaviour
 {
 
-    // animations in Update()
-    void Update()
+    [Header("Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    private float currentMoveDirection;
+
+    void Start()
     {
-        
+        if (rb == null) rb = GetComponent<Rigidbody2D>();
     }
 
-    // todo
+    void Update()
+    {
+        // Apply movement velocity
+        if (rb != null) 
+        {
+            Vector2 velocity = rb.linearVelocity;
+            velocity.x = currentMoveDirection * moveSpeed;
+            rb.linearVelocity = velocity;
+        }
+    }
+
+    public void Move(float direction)
+    {
+        currentMoveDirection = direction;
+    }
+
     public void Climb()
     {
-        
+        if (rb != null)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        else
+        {
+            Debug.Log("Jumping (No Rigidbody assigned)");
+        }
     }
 
     // todo
     public void Drag()
     {
-        
+        Debug.Log("Dragging");
     }
 
 }
--- Assets/Scripts/Players/ClimberController.cs
+++ Assets/Scripts/Players/ClimberController.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.DefaultInputActions;
 
+[RequireComponent(typeof(PlayerActions))]
 public class ClimberController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("References")]
+    [SerializeField] private PlayerActions playerActions;
+    [SerializeField] private ClimberActions ClimberActions;
+
+    [Space(10)]
+    [Header("Input Actions")]
+    [SerializeField] private InputActionReference Jump;
+    [SerializeField] private InputActionReference Move;
+    [SerializeField] private InputActionReference Drag;
+
+
     void Start()
     {
-        
+        Jump.action.Enable();
+        Move.action.Enable();
+        Drag.action.Enable();
+
+        ClimberActions = GetComponent<ClimberActions>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Jump.action.WasPressedThisFrame())
+        {
+            ClimberActions.Climb();
+        }
+
+        if (Drag.action.WasPressedThisFrame())
+        {
+            ClimberActions.Drag();
+        }
+
+        float moveDirection = Move.action.ReadValue<float>();
+        ClimberActions.Move(moveDirection);
+
     }
 }
