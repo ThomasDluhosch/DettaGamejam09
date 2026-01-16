b1702a488bab5827b221118675d1ecc1c7f1ce63
Mohamed Amin Ben Chrifa <mohamed6aminbenchrifa@gmail.com>
2026-01-16 22:22:39 +0100

    basic crane movement

--- Assets/Scripts/CraneSystem/CraneController.cs
+++ Assets/Scripts/CraneSystem/CraneController.cs
@@ -1,7 +1,36 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CraneController : MonoBehaviour
 {
+    [Header("Crane Components")]
+    [SerializeField] private Transform craneArm;
+    [SerializeField] private Transform startPoint;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float horizontalLimit = 8f;
+
+    [Header("Events")]
+    public UnityEvent onCranePickedUp;
+    public UnityEvent onCraneDropped;
+
+    private CraneInput craneInput;
+
+    private void Awake()
+    {
+        craneInput = new CraneInput();
+    }
+
+    private void OnEnable()
+    {
+        craneInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        craneInput.Disable();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +40,38 @@ public class CraneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = craneInput.Crane.Movement.ReadValue<float>();
+
+        HandleMovement(horizontal);
+
+        if (craneInput.Crane.Pickup.WasPerformedThisFrame())
+        {
+            onCranePickedUp.Invoke();
+        }
+        if (craneInput.Crane.Drop.WasPerformedThisFrame())
+        {
+            onCraneDropped.Invoke();
+        }
+    }
+
+    void HandleMovement(float horizontal)
+    {
+        if ((craneArm.position.x <= -horizontalLimit && horizontal < 0) ||
+            (craneArm.position.x >= horizontalLimit && horizontal > 0))
+        {
+            return;
+        }
+
+        craneArm.Translate(Vector3.right * horizontal * moveSpeed * Time.deltaTime);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (startPoint != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(new Vector3(-horizontalLimit, startPoint.position.y, startPoint.position.z),
+                            new Vector3(horizontalLimit, startPoint.position.y, startPoint.position.z));
+        }
     }
 }
