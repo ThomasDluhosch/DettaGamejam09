e4561580bfefe6116714560549cbc2ce6bbffabc
Tifu2000 <119350322+Tifu2000@users.noreply.github.com>
2026-01-17 17:24:22 +0100

    Powerups now delete themselfs after a delay

--- Assets/Scripts/Powerups/Powerup.cs
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -1,8 +1,10 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public abstract class Powerup : BaseBlock
 {
+    int durationTime = 5;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player")) {
@@ -10,6 +12,17 @@ public abstract class Powerup : BaseBlock
             Debug.Log("Activated Powerup");
             Destroy(gameObject);
         }
+    }
+
+    public override void OnCraneDrop() {
+        base.OnCraneDrop();
+        StartCoroutine(liveTimer());
+
+    }
+
+    private IEnumerator liveTimer() {
+        yield return new WaitForSeconds(durationTime);
+        Destroy(gameObject);
     }
 
     protected abstract void activatePowerup(GameObject player);
